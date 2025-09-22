8becfad217abc1f372d607edaa84344860d67931
Noah <98564568+THatMustacheCat@users.noreply.github.com>
2025-09-22 12:40:43 -0400

    lesss gooo

--- reverse_string.py
+++ reverse_string.py
@@ -1,10 +1,14 @@
-def reverse_string(text):
+def reverse_string(str1 = ""):
     # TODO create reverse string function
     # HINT: Use for i in range(len(text) - 1, -1, -1): to go backwards
     # HINT: Build reversed_text character by character: reversed_text += text[i]
     # HINT: Start with reversed_text = ""
     # HINT: Return the reversed_text
     # HINT: NO SLICING ALLOWED - must use loops only
+    newStr = ""
+    for i in range(1, len(str1) + 1):
+        newStr += str1[len(str1) - i]
+    return newStr
     pass
 
 if __name__ == "__main__":
